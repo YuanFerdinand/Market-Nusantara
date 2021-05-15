13988ad7d5ee6fbbc7129f8cfbaf50ee55a2f79e
YuanFerdinand <lionelkailuhu@gmail.com>
2021-05-15 11:34:44 +0700

    origin master

---
+++ lib/views/add_item_page.dart
@@ -0,0 +1,320 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:market_nusantara/views/login_page.dart';
+
+class AddItemPage extends StatefulWidget {
+  @override
+  _AddItemPageState createState() => _AddItemPageState();
+}
+
+class _AddItemPageState extends State<AddItemPage> {
+  String nama, merek, tipe;
+  var harga, jml;
+  String imagePath;
+  var imageDir;
+
+  Future<File> getImage() async {
+    var imageFile;
+    final picker = ImagePicker();
+    PickedFile pickedFile = await picker.getImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
+      return imageDir = imageFile;
+    } else {
+      return imageDir = imageFile;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Form(
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 1,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: <Color>[Color(0xff2CCACA), Color(0xffFF1192)])),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    padding: EdgeInsets.only(
+                      bottom: 5, // Space between underline and text
+                    ),
+                    // decoration: BoxDecoration(
+                    //     border: Border(
+                    //         bottom: BorderSide(
+                    //   color: Color(0xff8142FC),
+                    //   width: 5.0, // Underline thickness
+                    // ))),
+                    child: Text(
+                      "TAMBAH BARANG",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Popppins",
+                          fontSize: 24,
+                          fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                  Flexible(
+                    child: Container(
+                      padding: EdgeInsets.only(left: 15, right: 15),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20)),
+                          color: Colors.white),
+                      //0xff2CCACA : 0xff5D11FF
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      height: MediaQuery.of(context).size.height * 0.85,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "Nama",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xff2CCACA)),
+                                ),
+                              ),
+                              TextFormField(
+                                onChanged: (value) {
+                                  nama = value.trim();
+                                },
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.name,
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.only(left: 5),
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(50)),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    hintText: "Masukan nama Barang",
+                                    hintStyle: TextStyle(
+                                        fontFamily: 'Poppins', fontSize: 12)),
+                              )
+                            ],
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "Harga",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xff2CCACA)),
+                                ),
+                              ),
+                              TextFormField(
+                                onChanged: (value) {
+                                  harga = double.parse(value);
+                                },
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.number,
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.only(left: 5),
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(50)),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    hintText: "Masukan harga barang",
+                                    hintStyle: TextStyle(
+                                        fontFamily: 'Poppins', fontSize: 12)),
+                              )
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "Tipe",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xff2CCACA)),
+                                ),
+                              ),
+                              TextFormField(
+                                onChanged: (value) {
+                                  tipe = value.trim();
+                                },
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.name,
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.only(left: 5),
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(50)),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    hintText: "Masukan tipe barang",
+                                    hintStyle: TextStyle(
+                                        fontFamily: 'Poppins', fontSize: 12)),
+                              )
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "Merek",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xff2CCACA)),
+                                ),
+                              ),
+                              TextFormField(
+                                onChanged: (value) {
+                                  merek = value.trim();
+                                },
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.name,
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.only(left: 5),
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(50)),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    hintText: "Masukan tipe barang",
+                                    hintStyle: TextStyle(
+                                        fontFamily: 'Poppins', fontSize: 12)),
+                              )
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "Jumlah",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xff2CCACA)),
+                                ),
+                              ),
+                              TextFormField(
+                                onChanged: (value) {
+                                  jml = double.parse(value);
+                                },
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.number,
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.only(left: 5),
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(50)),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    hintText: "Masukan jumlah barang",
+                                    hintStyle: TextStyle(
+                                        fontFamily: 'Poppins', fontSize: 12)),
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                  decoration: BoxDecoration(
+                                      color: Color(0xff2CCACA),
+                                      borderRadius: BorderRadius.circular(50)),
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.05,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.43,
+                                  child: (imageDir == null)
+                                      ? Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            Text("Upload file",
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 18)),
+                                            IconButton(
+                                                icon: Icon(Icons.upload_file),
+                                                color: Colors.white,
+                                                iconSize: 30,
+                                                onPressed: () {
+                                                  getImage();
+                                                })
+                                          ],
+                                        )
+                                      : Text(
+                                          imageDir.toString(),
+                                          maxLines: 1,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12),
+                                        )),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                height:
+                                    MediaQuery.of(context).size.height * 0.05,
+                                width: MediaQuery.of(context).size.width * 0.43,
+                                child: Center(
+                                  child: Text(
+                                    "Tambah",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w900),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- lib/views/profil_page.dart
+++ lib/views/profil_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_nusantara/views/add_item_page.dart';
 import 'package:market_nusantara/views/login_page.dart';
 
 class ProfilPage extends StatefulWidget {
@@ -13,62 +14,141 @@ class _ProfilPageState extends State<ProfilPage> {
       appBar: AppBar(
         backgroundColor: Color(0xff2CCACA),
         title: Text("Profile"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Container(
-                margin: EdgeInsets.only(top: 60, bottom: 60),
-                width: MediaQuery.of(context).size.width * 0.5,
-                height: MediaQuery.of(context).size.height * 0.25,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    image: DecorationImage(
-                        image: AssetImage("assets/magnus.jpg"),
-                        fit: BoxFit.cover)),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.add,
+                size: 35,
               ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              margin: EdgeInsets.only(bottom: 30),
-              decoration: BoxDecoration(color: Color(0xffDAA520)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "  You're a Gold User!",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                color: Color(0xff2CCACA),
-              ),
-              child: Text(
-                "Magnus Carlsen",
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white),
-              ),
-            ),
-            RaisedButton(
               onPressed: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return AddItemPage();
+                }));
+              }),
+          IconButton(
+              icon: Icon(
+                Icons.exit_to_app,
+                size: 35,
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) {
                   return LoginPage();
                 }));
-              },
-              child: Text("LOGOUT"),
-            )
-          ],
-        ),
+              }),
+        ],
       ),
+      body: ListView(children: [
+        Center(
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Container(
+                  margin: EdgeInsets.only(top: 60, bottom: 30),
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      image: DecorationImage(
+                          image: AssetImage("assets/magnus.jpg"),
+                          fit: BoxFit.cover)),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.only(bottom: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: Color(0xff2CCACA),
+                ),
+                child: Text(
+                  "Magnus Carlsen",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: MediaQuery.of(context).size.width * 0.5,
+                margin: EdgeInsets.only(bottom: 70),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xffFF1192)),
+                child: Center(
+                  child: Text(
+                    " AIRO Corps. ",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                elevation: 7,
+                child: Container(
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Container(
+                                margin: EdgeInsets.only(top: 20, bottom: 10),
+                                height: 50,
+                                width: 50,
+                                child: Image(
+                                    image:
+                                        AssetImage("assets/belumdibayar.png"))),
+                            Text("Bayar")
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                                margin: EdgeInsets.only(top: 20, bottom: 10),
+                                height: 50,
+                                width: 50,
+                                child: Image(
+                                    image: AssetImage("assets/diproses.png"))),
+                            Text("Diproses")
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                                margin: EdgeInsets.only(top: 20, bottom: 10),
+                                height: 50,
+                                width: 50,
+                                child: Image(
+                                    image: AssetImage("assets/dikirim.jpg"))),
+                            Text("Dikirim")
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                                margin: EdgeInsets.only(top: 20, bottom: 10),
+                                height: 50,
+                                width: 50,
+                                child: Image(
+                                    image: AssetImage("assets/diterima.png"))),
+                            Text("Diterima")
+                          ],
+                        ),
+                      ]),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
