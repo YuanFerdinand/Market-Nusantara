38e47e4cb842812226d89f26810c4f564a6147be
arbihidayatullah <arbihidayatullah@gmail.com>
2021-07-17 20:45:23 +0700

    konfirmasi page

--- lib/views/Setting_page.dart
+++ lib/views/Setting_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:market_nusantara/locale/locale_keys.g.dart';
 import 'package:market_nusantara/views/about_page.dart';
+import 'package:market_nusantara/views/konfirmasibarang_page.dart';
 import 'package:market_nusantara/views/language_page.dart';
 import 'package:market_nusantara/views/profil_page.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -69,6 +70,29 @@ class _SettingPageState extends State<SettingPage> {
                   children: [
                     Text(
                       LocaleKeys.tentang,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey[600]),
+                    ).tr(),
+                    Icon(Icons.arrow_forward)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return KonfirmasiBarangPage();
+                  }));
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Konfirmasi Barang",
                       style: TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.w500,
